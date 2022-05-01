120beff7c720627d36f4724c4d02db384674c8a1
Mihail <mishkagoo@gmail.com>
2022-05-01 21:02:52 +0300

    Created class component with state

--- src/components/header/app-info/app-info.js
+++ src/components/header/app-info/app-info.js
@@ -1,25 +1,44 @@
+import { Component } from 'react'
 import logoAsTec from '../../../img/logo-as-tec.png'
 import emailLogo from '../../../img/e-mail-as-tec.png'
 
 import './app-info.css'
 
 
-const AppInfo = () => {
-    return (
-        <div className="head">
-            <img className="app-logo" src={logoAsTec} alt="Company logo" />
-            <div className="title-site">
-                <h1>Компанія AS-TEC</h1>
+
+class AppInfo extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            peoples: 0
+        }
+    }
+
+    onLinkClick = () => {
+        this.setState(state => ({
+            peoples: state.peoples + 1
+        }))
+    }
+
+    render() {
+
+        return (
+            <div className="head">
+                <img className="app-logo" src={logoAsTec} alt="Company logo" />
+                <div className="title-site">
+                    <h1>Компанія AS-TEC</h1>
+                </div>
+                <div className="mail-logo">
+                    <img src={emailLogo} width="64px" alt="e-mail-logo" />
+                </div>
+                <div className="contact-info">
+                    <a href="tel:+380661234567" onClick={this.onLinkClick}>Tel: +38 066-123-45-67</a><br />
+                    <a href="mailto:mail@gmail.com" onClick={this.onLinkClick}>E-Mail : mail@gmail.com</a>
+                    <p>Успішних угод сьогодні: {this.state.peoples}</p>
+                </div>
             </div>
-            <div className="mail-logo">
-                <img src={emailLogo} width="64px" alt="e-mail-logo" />
-            </div>
-            <div className="contact-info">
-                <a href="tel:+380661234567">Tel: +38 066-123-45-67</a><br />
-                <a href="email:mail@gmail.com">E-Mail : mail@gmail.com</a>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default AppInfo
